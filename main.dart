8ad7458f77634074e98f7fbe01d13d7c84b45fff
zunnn <165063576+scchanz@users.noreply.github.com>
2024-10-18 09:22:19 +0700

    flutter1.1

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp()); //memanggil fungsi runAPP (menjalankan seluruh apk di dalam myAPP)
+}
+
+class MyApp extends StatelessWidget { //membuat apk abstrak dari statelessWidget (template apk), apk nya bernama MyAPP
+  const MyApp({super.key}); //Menunjukan bahwa apk ini akan tetap, tidak dapat berubah setalah build
+
+  @override //mengganti nilai lama yg sudah ada di template, dengan nilai-nilai yg baru (replace/over write)
+  Widget build(BuildContext context) { //fungsi build yg membangun UI (mengatur posisi widget)
+    return ChangeNotifierProvider(  //ChangeNotifierProvider mendengarkan/mendeteksi semua interaksi yang terjadi di apk(like pada post)
+      create: (context) => 
+      MyAppState(), //s
+      child: MaterialApp( //pada state ini, menggunakan style desain materialUI
+        title: 'Namer App', //diberi judul (NamerApp)
+        theme: ThemeData( //data tema apklikasi, diberi warna (deepOrange)
+          useMaterial3: true, //versi MAterialUI yang dipakai versi 3
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+ //mendefinisikan MyAppstate
+class MyAppState extends ChangeNotifier { //state MyAppState diisi dengan 2 kata random yang digabungkan. kata random tsb disimpan di variable WordPair
+  var current = WordPair.random();
+}
+
+//membuat layout pada halaman HomePage
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>(); //widget menggunakan state MyAppState
+
+    //di bawah ini adalah kode program untuk menyusun layout
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase), //mengambil random text dari appstate pada variable WordPair current, lalu diubah menjadi huruf kecil semua
+           ElevatedButton( //membuat button timbul di dalam body
+            onPressed: () { //fungsi yang dieksekusi ketika button ditekan
+              print('button pressed!'); //tampilkan teks 'button prissed' di terminal saat button ditekan
+            },
+            child: Text('Next'), //berikan teks 'next' pada button (sebagai child)
+          ),
+        ],
+      ),
+    );
+  }
+}
