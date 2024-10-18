dd6d5b386febfa04e00f7c686037b5a4f0f395ec
zunnn <165063576+scchanz@users.noreply.github.com>
2024-10-18 09:49:02 +0700

    flutter1.2

--- main.dart
+++ main.dart
@@ -29,6 +29,10 @@ class MyApp extends StatelessWidget { //membuat apk abstrak dari statelessWidget
  //mendefinisikan MyAppstate
 class MyAppState extends ChangeNotifier { //state MyAppState diisi dengan 2 kata random yang digabungkan. kata random tsb disimpan di variable WordPair
   var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 //membuat layout pada halaman HomePage
@@ -45,7 +49,7 @@ class MyHomePage extends StatelessWidget {
           Text(appState.current.asLowerCase), //mengambil random text dari appstate pada variable WordPair current, lalu diubah menjadi huruf kecil semua
            ElevatedButton( //membuat button timbul di dalam body
             onPressed: () { //fungsi yang dieksekusi ketika button ditekan
-              print('button pressed!'); //tampilkan teks 'button prissed' di terminal saat button ditekan
+              appState.getNext();
             },
             child: Text('Next'), //berikan teks 'next' pada button (sebagai child)
           ),
